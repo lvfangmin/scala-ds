5c1a8c2d472e959a89733263500b1ae21533db8e
lvfangmin <lvfangmin@gmail.com>
2014-11-19 23:30:25 +0800

    update Stack datastructure

--- src/main/scala/stack/Stack.scala
+++ src/main/scala/stack/Stack.scala
@@ -1,4 +1,4 @@
-package Stack
+package stack
 
 class Stack[A] extends Iterator[A] {
   var N: Int = 0
@@ -46,6 +46,48 @@ class Stack[A] extends Iterator[A] {
 class Node[A](v: A) {
   var value: A = v
   var next: Node[A] = _
+}
+
+/**
+ * Another version, create stack based on List
+ */
+class Stack[+A](self: List[A]) {
+
+  // The top of this stack.
+  def top: A = self.head
+
+  // The rest of this stack.
+  def rest: Stack[A] = new Stack(self.tail)
+
+  // Checks whether this stack is empty or not.
+  def isEmpty: Boolean = self.isEmpty
+
+  /**
+   * Pops top element from this stack.
+   */
+  def pop: (A, Stack[A]) = (top, rest)
+
+  /**
+   * Pushes given element 'x' into this stack.
+   */
+  def push[B >: A](x: B): Stack[B] = new Stack(x :: self)
+}
+
+object Stack {
+
+   /**
+    * Returns an empty stack instance.
+    */
+   def empty[A]: Stack[A] = new Stack(Nil)
+
+   /**
+    * Creates a new stack from given 'xs' sequence.
+    */
+   def apply[A](xs: A*): Stack[A] = {
+    var r: Stack[A] = Stack.empty
+    for (x <- xs) r = r.push(x)
+    r
+  }
 }
 
 object Stack extends App {
